ce787e2b61b494a3064cfb62136b2bcf671c5c4e
AbrorCPP <abrorboltayev4843@gmail.com>
2026-02-08 21:37:53 +0500

    1

---
+++ utils/db_api/db.py
@@ -0,0 +1,53 @@
+import pymysql
+
+
+class Database:
+    def __init__(self, db_name, db_password, db_user, db_port, db_host):
+        self.db_name = db_name
+        self.db_password = db_password
+        self.db_user = db_user
+        self.db_port = db_port
+        self.db_host = db_host
+
+    def connect(self):
+        return pymysql.Connection(
+            database=self.db_name,
+            user=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    def execute(self, sql: str, params: tuple = (), commit=False, fetchone=False, fetchall=False) -> dict | list:
+        database = self.connect()
+        cursor = database.cursor()
+
+        cursor.execute(sql, params)
+        data = None
+
+        if fetchone:
+            data = cursor.fetchone()
+
+        elif fetchall:
+            data = cursor.fetchall()
+
+        if commit:
+            database.commit()
+
+        return data
+
+    def create_users_table(self) -> None:
+        """
+        Creates users table
+        :return: None
+        """
+        sql = """
+            CREATE TABLE IF NOT EXISTS users(
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                telegram_id INT NOT NULL UNIQUE,
+                fullname VARCHAR(100),
+                username VARCHAR(100)
+            )
+        """
+        self.execute(sql)
